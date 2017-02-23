ee8e07eac58c850b97757cfb785faf305506d57c
thomasjohnson <tom.johnson@donuts.co>
2017-02-22 16:45:35 -0800

    cleanup of code format

--- src/app/validators/customValidator.ts
+++ src/app/validators/customValidator.ts
@@ -1,4 +1,4 @@
-import {ValidatorFn, AbstractControl, FormControl} from "@angular/forms";
+import {ValidatorFn, AbstractControl, FormControl} from '@angular/forms';
 
 function isEmptyInputValue(value: any): boolean {
   // we don't check for string here so it also works with arrays
@@ -19,7 +19,7 @@ export class CustomValidator {
       if (isEmptyInputValue(control.value)) {
         return null;  // don't validate empty values to allow optional controls
       }
-      if (control.value < minAmount){
+      if (control.value < minAmount) {
         return {'minAmount': {'requiredAmount': minAmount, 'actualAmount': control.value}};
       } else if (control.value > maxAmount) {
         return {'maxAmount': {'requiredAmount': maxAmount, 'actualAmount': control.value}};
@@ -34,13 +34,13 @@ export class CustomValidator {
    * @param control
    * @returns {any}
    */
-  static validateEmailAddress(control: FormControl) : ValidationResult  {
-      let EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+  static validateEmailAddress(control: FormControl): ValidationResult {
+    const EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
 
-      if (!EMAIL_REGEXP.test(control.value)) {
-        return {invalidEmail: true};
-      }
-      return null;
+    if (!EMAIL_REGEXP.test(control.value)) {
+      return {invalidEmail: true};
+    }
+    return null;
   }
 
   /**
@@ -49,9 +49,9 @@ export class CustomValidator {
    * @param control
    * @returns {any}
    */
-  static validateipV4RegEx(control: FormControl) : ValidationResult  {
-    var v4RegEx = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-    if (!v4RegEx.test(control.value)){
+  static validateipV4RegEx(control: FormControl): ValidationResult {
+    const v4RegEx = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    if (!v4RegEx.test(control.value)) {
       return {invalidipV4RegEx: true};
     }
     return null;
@@ -63,9 +63,9 @@ export class CustomValidator {
    * @param control
    * @returns {any}
    */
-  static validateipV6RegEx(control: FormControl) : ValidationResult  {
-    var v6RegEx = /^(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}$/;
-    if (!v6RegEx.test(control.value)){
+  static validateipV6RegEx(control: FormControl): ValidationResult {
+    const v6RegEx = /^(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}$/;
+    if (!v6RegEx.test(control.value)) {
       return {invalidipV6RegEx: true};
     }
     return null;
