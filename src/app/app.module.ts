0ca2efb5b05c0d081c705e8b52a164762a5bec92
thomasjohnson <tom.johnson@donuts.co>
2017-02-22 16:46:21 -0800

    cleanup of code format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import {BrowserModule} from "@angular/platform-browser";
-import {NgModule} from "@angular/core";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
-import {AppComponent} from "./app.component";
-import {NavbarComponent} from "./navbar/navbar.component";
-import {SearchBarComponent} from "./search-bar/search-bar.component";
-import {RouterModule} from "@angular/router";
-import {SearchComponent} from "./search/search.component";
-import {SearchService} from "./service/search.service";
-import {DashboardComponent} from "./dashboard/dashboard.component";
-import {TldsIndexComponent} from "./tlds/tlds-index/tlds-index.component";
-import {TldService} from "./service/tld.service";
-import {TldsCreateComponent} from "./tlds/tlds-create/tlds-create.component";
-import {DialogModule} from "primeng/components/dialog/dialog";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {SearchBarComponent} from './search-bar/search-bar.component';
+import {RouterModule} from '@angular/router';
+import {SearchComponent} from './search/search.component';
+import {SearchService} from './service/search.service';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {TldsIndexComponent} from './tlds/tlds-index/tlds-index.component';
+import {TldService} from './service/tld.service';
+import {TldsCreateComponent} from './tlds/tlds-create/tlds-create.component';
+import {DialogModule} from 'primeng/components/dialog/dialog';
 
 @NgModule({
   declarations: [
