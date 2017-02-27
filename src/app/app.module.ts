522c394ab350eb2191873c4ed5f1370741aca049
thomasjohnson <tom.johnson@donuts.co>
2017-02-27 08:23:21 -0800

    Forgot to add permission model to Git. Updated Angular cli.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,21 +15,6 @@ import {TldsCreateComponent} from "./tlds/tlds-create/tlds-create.component";
 import {DialogModule} from "primeng/components/dialog/dialog";
 import {PermissionService} from "./service/Permission.service";
 import {AppRoutingModule} from "./app-routing.module";
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SearchBarComponent } from './search-bar/search-bar.component';
-import { RouterModule } from '@angular/router';
-import { SearchComponent } from './search/search.component';
-import { SearchService } from './service/search.service';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { TldsIndexComponent } from './tlds/tlds-index/tlds-index.component';
-import { TldService } from './service/tld.service';
-import { TldsCreateComponent } from './tlds/tlds-create/tlds-create.component';
-import { DialogModule } from 'primeng/components/dialog/dialog';
 import { LoginComponent } from './login/login.component';
 import { OAuthModule } from 'angular-oauth2-oidc';
 import { SessionService } from './service/session.service';
@@ -77,7 +62,7 @@ import { HttpClient } from './shared/http.client';
       }
     ])
   ],
-  providers: [SearchService, TldService, SessionService, MeService, HttpClient],
+  providers: [SearchService, TldService, PermissionService, SessionService, MeService, HttpClient],
   entryComponents: [],
   bootstrap: [AppComponent]
 })
