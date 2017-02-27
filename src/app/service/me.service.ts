cc3b684c01e87e95ae3a1a8162d629680f6537c0
thomasjohnson <tom.johnson@donuts.co>
2017-02-27 13:23:37 -0800

    added blank line between copyright and imports

--- src/app/service/me.service.ts
+++ src/app/service/me.service.ts
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 import { Injectable } from "@angular/core";
 import { HttpClient } from "../shared/http.client";
 import { User } from "../model/user.model";
