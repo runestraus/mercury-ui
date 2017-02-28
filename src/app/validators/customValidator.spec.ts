714d9190cd7b5776b67d037fa25bffaef9d5e73c
thomasjohnson <tom.johnson@donuts.co>
2017-02-28 09:15:31 -0800

    Added copyright and fixed tests for Money validtor.

--- src/app/validators/customValidator.spec.ts
+++ src/app/validators/customValidator.spec.ts
@@ -1,16 +1,36 @@
+// Copyright 2017 Donuts Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import {FormControl } from '@angular/forms';
 import {CustomValidator} from "./customValidator";
 
 describe('customValidator Test', () => {
 
-  xit('should validate Money is not valid', () => {
-    expect(CustomValidator.validateMoney(1,2).prototype(new FormControl('1'))).toEqual({
-      'validateipV6RegEx': {'valid': false}
-    });
+  it('should validate Money maximum amount is not valid', () => {
+    expect(CustomValidator.validateMoney(1,2)(new FormControl('3'))).toEqual(
+      { MaximumAmount: Object({ requiredAmount: 2, actualAmount: '3' }) }
+    );
   });
 
-  xit('should validate Money with null response', () => {
-    expect(CustomValidator.validateMoney(1,3)).toEqual(null);
+  it('should validate Money minimum amount is not valid', () => {
+    expect(CustomValidator.validateMoney(2,4)(new FormControl('1'))).toEqual(
+      { MinimumAmount: Object({ requiredAmount: 2, actualAmount: '1' }) }
+    );
+  });
+
+  it('should validate Money with null response', () => {
+    expect(CustomValidator.validateMoney(1,3)(new FormControl('2'))).toEqual(null);
   });
 
   it('should validate ipV6 address is not valid', () => {
--- src/app/validators/customValidator.ts
+++ src/app/validators/customValidator.ts
@@ -1,3 +1,17 @@
+// Copyright 2017 Donuts Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import {ValidatorFn, AbstractControl, FormControl} from '@angular/forms';
 
 function isEmptyInputValue(value: any): boolean {
@@ -20,9 +34,9 @@ export class CustomValidator {
         return null;  // don't validate empty values to allow optional controls
       }
       if (control.value < minAmount) {
-        return {'minAmount': {'requiredAmount': minAmount, 'actualAmount': control.value}};
+        return {'MinimumAmount': {'requiredAmount': minAmount, 'actualAmount': control.value}};
       } else if (control.value > maxAmount) {
-        return {'maxAmount': {'requiredAmount': maxAmount, 'actualAmount': control.value}};
+        return {'MaximumAmount': {'requiredAmount': maxAmount, 'actualAmount': control.value}};
       }
       return null;
     };
